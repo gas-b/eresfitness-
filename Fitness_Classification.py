19c8b590599cfc59aacdacfb3c05467fa50b655b
gas-b <a01615309@tec.mx>
2025-11-27 22:36:27 -0600

    Update Fitness_Classification.py

--- Fitness_Classification.py
+++ Fitness_Classification.py
@@ -7,32 +7,45 @@ from sklearn.tree import DecisionTreeClassifier
 
 st.write(''' # Predicción de vida fitness ''')
 st.image("ejemplo_a_seguir.jpg", caption="Introduce tus datos para saber si llevas una vida fitness, o no")
-
 st.header('Datos de evaluación')
 
 def user_input_features():
-  # Entrada
   age = st.number_input('age:', min_value=1, max_value=100, value = 1, step = 1)
   height_cm = st.number_input('height_cm:', min_value=1, max_value=240, value = 1, step = 1)
   weight_kg = st.number_input('weight_kg:', min_value=1, max_value=200, value = 1, step = 1)
-  Heart_rate = st.number_input('Heart_rate:',min_value=1, max_value=160, value = 1, step = 1)
+  heart_rate = st.number_input('heart_rate:', min_value=1, max_value=160, value = 1, step = 1)
   blood_pressure = st.number_input('blood_pressure:', min_value=1, max_value=150, value = 1, step = 1)
   nutrition_quality = st.number_input('nutrition_quality:', min_value=1, max_value=10, value = 1, step = 1)
   activity_index = st.number_input('activity_index:', min_value=1, max_value=4, value = 1, step = 1)
   gender = st.number_input('gender:', min_value=0, max_value=1, value = 0, step = 1)
 
-  user_input_data = {'age': age,
-                     'gender': gender,
-                     'height_cm': height_cm,
-                     'weight_kg': weight_kg,
-                     'Heart_rate': Heart_rate,
-                     'blood_pressure': blood_pressure,
-                     'nutrition_quality': nutrition_quality,
-                     'activity_index': activity_index,}
+  user_input_data = {
+      'age': age,
+      'height_cm': height_cm,
+      'weight_kg': weight_kg,
+      'heart_rate': heart_rate,
+      'blood_pressure': blood_pressure,
+      'nutrition_quality': nutrition_quality,
+      'activity_index': activity_index,
+      'gender': gender
+  }
 
-  features = pd.DataFrame(user_input_data, index=[0])
+  df = pd.DataFrame(user_input_data, index=[0])
 
-  return features
+  expected_order = [
+      'age',
+      'height_cm',
+      'weight_kg',
+      'heart_rate',
+      'blood_pressure',
+      'nutrition_quality',
+      'activity_index',
+      'gender'
+  ]
+
+  df = df[expected_order]
+  return df
+
 
 df = user_input_features()
 
@@ -40,7 +53,13 @@ is_fit =  pd.read_csv('Fitness_Classification2.csv', encoding='latin-1')
 X = is_fit.drop(columns='is_fit')
 Y = is_fit['is_fit']
 
-classifier = DecisionTreeClassifier(max_depth=6, criterion='entropy', min_samples_leaf=25, max_features=7, random_state=0)
+classifier = DecisionTreeClassifier(
+    max_depth=6,
+    criterion='entropy',
+    min_samples_leaf=25,
+    max_features=7,
+    random_state=0
+)
 classifier.fit(X, Y)
 
 prediction = classifier.predict(df)
